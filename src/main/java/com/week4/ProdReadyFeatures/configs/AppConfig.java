e3b627aa40e53e88bd7adafd7ba6d8da20eca3fe
Rahul Mahato <mahatorahul2901@gmail.com>
2025-05-11 14:45:04 +0530

    basic-setup done

--- src/main/java/com/week4/ProdReadyFeatures/configs/AppConfig.java
+++ src/main/java/com/week4/ProdReadyFeatures/configs/AppConfig.java
@@ -1,4 +1,14 @@
 package com.week4.ProdReadyFeatures.configs;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class AppConfig {
+
+    @Bean
+    ModelMapper getModelMapper(){
+        return new ModelMapper();
+    }
 }
