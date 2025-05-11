7b3941f4d1004042ee7ac4b8c9174272154f8f3a
Rahul Mahato <mahatorahul2901@gmail.com>
2025-05-11 14:42:45 +0530

    ++repository

--- src/main/java/com/week4/ProdReadyFeatures/repositories/PostRepository.java
+++ src/main/java/com/week4/ProdReadyFeatures/repositories/PostRepository.java
@@ -1,4 +1,9 @@
 package com.week4.ProdReadyFeatures.repositories;
 
-public interface PostRepository {
+import com.week4.ProdReadyFeatures.entities.PostEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<PostEntity, Long> {
 }
