b41a5eb29b79cafdc9b92d377e5096c6bd47340f
Rahul Mahato <mahatorahul2901@gmail.com>
2025-05-11 12:18:42 +0530

    first commit

---
+++ src/test/java/com/week4/ProdReadyFeatures/ProductionReadyFeaturesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.week4.ProdReadyFeatures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductionReadyFeaturesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
