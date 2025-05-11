52f915f2ef3ee72000af5cab1d15bfef5bc676a8
Rahul Mahato <mahatorahul2901@gmail.com>
2025-05-11 14:44:42 +0530

    ++controller

--- src/main/java/com/week4/ProdReadyFeatures/controllers/PostController.java
+++ src/main/java/com/week4/ProdReadyFeatures/controllers/PostController.java
@@ -1,4 +1,26 @@
 package com.week4.ProdReadyFeatures.controllers;
 
+import com.week4.ProdReadyFeatures.dto.PostDTO;
+import com.week4.ProdReadyFeatures.services.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/posts")
+@RequiredArgsConstructor
 public class PostController {
+
+    private final PostService postService;
+
+    @GetMapping
+    public List<PostDTO> getAllPost(){
+        return postService.getAllPost();
+    }
+
+    @PostMapping
+    public PostDTO createPost(@RequestBody PostDTO inputPost){
+        return postService.createNewPost(inputPost);
+    }
 }
