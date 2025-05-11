afcc52981100df4ef34df849af237c2ef0ebc91b
Rahul Mahato <mahatorahul2901@gmail.com>
2025-05-11 14:43:32 +0530

    ++service

--- src/main/java/com/week4/ProdReadyFeatures/services/PostService.java
+++ src/main/java/com/week4/ProdReadyFeatures/services/PostService.java
@@ -1,4 +1,10 @@
 package com.week4.ProdReadyFeatures.services;
 
+import com.week4.ProdReadyFeatures.dto.PostDTO;
+
+import java.util.List;
+
 public interface PostService {
+    PostDTO createNewPost(PostDTO inputPost);
+    List<PostDTO> getAllPost();
 }
--- src/main/java/com/week4/ProdReadyFeatures/services/PostServiceImp.java
+++ src/main/java/com/week4/ProdReadyFeatures/services/PostServiceImp.java
@@ -1,4 +1,33 @@
 package com.week4.ProdReadyFeatures.services;
 
-public class PostServiceImp {
+import com.week4.ProdReadyFeatures.dto.PostDTO;
+import com.week4.ProdReadyFeatures.entities.PostEntity;
+import com.week4.ProdReadyFeatures.repositories.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class PostServiceImp implements PostService {
+
+    private final PostRepository postRepository;
+    private final ModelMapper modelMapper;
+
+    @Override
+    public PostDTO createNewPost(PostDTO inputPost) {
+        PostEntity postEntity = modelMapper.map(inputPost, PostEntity.class);
+        return modelMapper.map(postRepository.save(postEntity), PostDTO.class);
+    }
+
+    @Override
+    public List<PostDTO> getAllPost() {
+        return postRepository.findAll()
+                .stream()
+                .map(postEntity -> modelMapper.map(postEntity, PostDTO.class))
+                .collect(Collectors.toList());
+    }
 }
